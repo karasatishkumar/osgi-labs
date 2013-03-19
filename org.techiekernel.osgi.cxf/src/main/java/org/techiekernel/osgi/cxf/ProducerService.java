db2f3ae318873777c83fb14421c5ae8a78797c2a
satish <karasatishkumar@gmail.com>
2013-03-19 21:44:47 +0530

    playing arround cxf, but no luck

--- org.techiekernel.osgi.cxf/src/main/java/org/techiekernel/osgi/cxf/ProducerService.java
+++ org.techiekernel.osgi.cxf/src/main/java/org/techiekernel/osgi/cxf/ProducerService.java
@@ -3,13 +3,19 @@ package org.techiekernel.osgi.cxf;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.ProduceMime;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+
 @Path("/producer")
-public interface ProducerService {
+public class ProducerService{
+
 	@GET
 	@Path("/{param}")
-	@ProduceMime("application/json")
-	public Response getMsg(@PathParam("param") String msg); 
+	@Produces("application/json")
+	public Response getMsg(@PathParam("param") String msg) {
+		// TODO Auto-generated method stub
+		return Response.status(200).entity(msg + " has been produced.").build();
+	}
+
 }
