27930a7276867ea1d998c799481025ba89923fbc
satish <karasatishkumar@gmail.com>
2013-03-27 18:20:46 +0530

    feature project

---
+++ org.techiekernel.osgi.feature/src/main/java/org/techiekernel/osgi/App.java
@@ -0,0 +1,13 @@
+package org.techiekernel.osgi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
